f92ac117f157a2bd7a5184f7936f9e1a0317bf30
Beemeo17 <127359555+Beemeo17@users.noreply.github.com>
2024-01-19 07:07:30 +0700

    Abeebot.py

--- Abeebot.py
+++ Abeebot.py
@@ -1,8 +1,19 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
+import sqlite3
 import asyncio
 import genshin
+import calendar
+import requests
+
+conn = sqlite3.connect('templates/users.db')
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE IF NOT EXISTS users
+             (id INTEGER PRIMARY KEY, name TEXT, exp INTEGER, level INTEGER, explevel INTEGER)'''
+          )
 
 intents = discord.Intents.all()
 intents.members = True
@@ -13,8 +24,11 @@ bot = commands.Bot(command_prefix='+', intents=intents, help_command=None)
 # bot on
 @bot.event
 async def on_ready():
-  print("DISCORD ON BOT!")
-  await run_data()
+  update_data.start()
+  url.start()
+  print("█▄▄ █▀█ ▀█▀   ▄▀█   █▀ ▄▀█ █▄░█   █▀ ▄▀█ █▄░█ █▀▀")
+  print("█▄█ █▄█ ░█░   █▀█   ▄█ █▀█ █░▀█   ▄█ █▀█ █░▀█ █▄█")
+  await update_presence()
   try:
     synced = await bot.tree.sync()
     print(
@@ -31,8 +45,8 @@ async def on_ready():
     elif current_time.hour == 7 and current_time.minute == 0:
       await channel.send(
         "chúc mọi người một ngày mới vui vẻ 🧩**Good Morning**🧩")
-    elif current_time.hour == 23 and current_time.minute == 7 :
-      await dailys()
+    elif current_time.hour == 23 and current_time.minute == 5 :
+      await mainss()
     await asyncio.sleep(60)
 
 
@@ -62,7 +76,7 @@ async def daily_reward(client):
     except Exception as e:
       await channel.send(f"lỗi trong điểm danh {gamess}: {e} | {datasss.nickname}")
       
-async def dailys(*cookies_data):
+async def mainss(*cookies_data):
   cookies_data = [
     {
       "ltuid": 139734936,
@@ -91,66 +105,121 @@ async def on_command_error(ctx, error):
   raise error
 
 
-#===================================
-
-async def run_data():
-  client1 = genshin.Client(
-    {
-      "ltuid_v2": 139734936,
-      "ltoken_v2":
-      'v2_CAISDGM5b3FhcTNzM2d1OBokYWRmZDE3ZTMtMDc1Ny00ZTI2LTgxY2ItZDhhY2Y2MTYxNGY3IMfa96cGKL_NlzEwmN_QQg==',
-      "ltmid_v2": '1kvjdm9qmp_hy',
-      "cookie_token_v2":
-      'v2_CAQSDGM5b3FhcTNzM2d1OBokYWRmZDE3ZTMtMDc1Ny00ZTI2LTgxY2ItZDhhY2Y2MTYxNGY3IMfa96cGKJbYt-EFMJjf0EI=',
-      "account_mid_v2": '1kvjdm9qmp_hy',
-      "account_id_v2": 139734936
-    },
-    lang="vi-vn")
-
-  data1 = await client1.get_hoyolab_user()
-  
-
-  signed_ins1, claimed_rewardss1 = await client1.get_reward_info(game=genshin.types.Game.STARRAIL)
-
-  signed_in1, claimed_rewards1 = await client1.get_reward_info(
-    game=genshin.types.Game.GENSHIN)
-
-  dailys1 = f'{data1.nickname}-GI<{signed_in1}•{claimed_rewards1}>+HSR<{signed_ins1}•{claimed_rewards1}>'
-
-  client2 = genshin.Client(
-    {
-      "ltuid": 89595259,
-      "ltoken": 'W26k5CAFSzoR5UHCXMNXBoLdgWahoe2VNIKEIsjb'
-    },
-    lang="vi-vn")
-
-  data2 = await client2.get_hoyolab_user()
-  
-  signed_in2, claimed_rewards2 = await client2.get_reward_info(
-    game=genshin.types.Game.GENSHIN)
- 
-  signed_ins2, claimed_rewardss2 = await client2.get_reward_info(
-  game=genshin.types.Game.STARRAIL)
-
-  dailys2 = f'{data2.nickname}-GI<{signed_in2}•{claimed_rewards2}>+HSR<{signed_ins2}•{claimed_rewards2}>'
-
-  
+async def update_presence():
   global start_time
+
   start_time = datetime.now()
-  game = discord.Game(name=f"DAILY\n {dailys1} \n {dailys2}")
+  game = discord.Game(name="❄️HIVE Teyvat❄️")
   await bot.change_presence(activity=game)
-#===================================
+
+
+@tasks.loop(seconds=60)
+async def url():
+  url = ["https://e5f771e1-0455-4671-9768-7797608e554e-00-1vg3ablsvxklf.global.replit.dev/"]
+  try:
+    for TAH in url:
+      response = requests.get(TAH)
+  except requests.exceptions.RequestException as e:
+    print(f"Error accessing website: {e}")
+
+
+import pytz
+from datetime import datetime, time
+
+tz = pytz.timezone('Asia/Ho_Chi_Minh')
+
+
+@tasks.loop(seconds=10)
+async def update_data():
+  try:
+    guild = bot.get_guild(550601755709407233)
+    channel = guild.get_channel(1105730219110825984)
+
+    boosts = guild.premium_subscription_count
+    current_time = datetime.now(tz).strftime('%H:%M:%S - %d/%m/%Y')
+    online_members = sum(1 for member in guild.members
+                         if member.status != discord.Status.offline)
+    total_members = guild.member_count
+    bot_count = sum(1 for member in guild.members if member.bot)
+
+    embed = discord.Embed(title="**Thông tin server**",
+                          color=discord.Color.blue())
+    embed.add_field(name=f"> Số Người Đang Online: {str(online_members)}",
+                    value="",
+                    inline=False)
+    embed.add_field(name=f"> Tổng Thành Viên: {str(total_members)}",
+                    value="",
+                    inline=False)
+    embed.add_field(
+      name=f"> Thành Viên: {str(((total_members)) - ((bot_count)))}",
+      value="",
+      inline=False)
+    embed.add_field(name=f"> Số Lượng Bot: {str(bot_count)}",
+                    value="",
+                    inline=False)
+    embed.add_field(name=f"> Boost: {str(boosts)}", value="", inline=False)
+
+    c.execute(
+      "SELECT name, level, exp FROM users ORDER BY level DESC, exp DESC LIMIT 3"
+    )
+    results = c.fetchall()
+
+    embed.add_field(name="**Bảng Xếp Hạng**", value="", inline=False)
+    embed.set_thumbnail(url="https://example.com/leaderboard_icon.png")
+
+    for index, result in enumerate(results):
+      name, level, exp = result
+      message_count = exp - 1
+      embed.add_field(name=f"> #{index+1} - {name}",
+                      value=f"> Level: **{level}**   EXP: **{message_count}**",
+                      inline=False)
+      embed.set_footer(text=f"UPDATE: {current_time}")
+
+    message = await channel.fetch_message(1111143360707711016)
+    await message.edit(content=None, embed=embed)
+
+  except Exception as e:
+    return e
+
+
+#setlevel
+@bot.tree.command(name="uplevel", description="setlevel")
+async def uplevel(Interaction, member: discord.Member, level: int):
+  if not discord.utils.get(Interaction.user.roles, id=594761273200082955):
+    await Interaction.channel.send("Bạn không có quyền sử dụng lệnh này.")
+    return
+
+  mentioned_user = member
+  exp = 0
+  c.execute("UPDATE users SET level=?, exp=? WHERE id=?",
+            (level, exp, mentioned_user.id))
+  conn.commit()
+
+  await Interaction.response.send_message(
+    f"Đã cập nhật level của {mentioned_user.mention} thành {level}.")
+  new_nickname = f"[TT{level}] {mentioned_user.name}"
+  try:
+    await mentioned_user.edit(nick=new_nickname)
+  except discord.Forbidden:
+    return
+
+
+current_time = datetime.now()
+vn_timezone = pytz.timezone('Asia/Ho_Chi_Minh')
+
 
 import keep_alive
 keep_alive.keep_alive()
 
-import pytz
-from datetime import datetime, time
-current_time = datetime.now()
-tz = pytz.timezone('Asia/Ho_Chi_Minh')
+
+async def load():
+  for filename in os.listdir('./cog'):
+    if filename.endswith(".py"):
+      await bot.load_extension(f'cog.{filename[:-3]}')
 
 
 async def main():
+  await load()
   await bot.start(os.getenv ("TOKEN"))
 
 
