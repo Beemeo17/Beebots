71ba88b6d0295c7ed6d58203d2f891a625d9a5d2
Beemeo17 <127359555+Beemeo17@users.noreply.github.com>
2024-01-19 15:37:55 +0700

    Update scuid.py

--- cog/scuid.py
+++ cog/scuid.py
@@ -408,6 +408,7 @@ class scuids(commands.Cog):
   async def scuid(self, Interaction, UID: int):
     try:
         UID = uid
+        await Interaction.channel.send(uid)
         data = await self.Client.fetch_user(uid)
         global_data["data"] = data
         global_data["uid"] = uid
