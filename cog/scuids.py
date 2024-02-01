05bdfe97c6626e1617f0ad0ecb21262e6550e237
Beemeo17 <127359555+Beemeo17@users.noreply.github.com>
2024-02-02 02:47:43 +0700

    scuids.py

--- cog/scuids.py
+++ cog/scuids.py
@@ -227,7 +227,7 @@ class Select(discord.ui.Select):
         messaget = await channel.send(file=file)
         file_url = messaget.attachments[0]
         embed = discord.Embed(color=discord.Color.dark_theme(), timestamp=datetime.now())
-        embed.add_field(name=f"Name.{charactert.name}", value=f"Level.{charactert.level} \nnguyên tố.{charactert.element} C.{charactert.constellations} \nrate.{'⭐'*charactert.rarity}", inline=False)
+        embed.add_field(name=f"Name.{charactert.name}", value=f"Level.{charactert.level} \nnguyên tố.{charactert.element} C.{len(charactert.constellations)}", inline=False)
         embed.set_image(url=file_url)   
         embed.set_thumbnail(url=f"{charactert.icon}")
         embed.set_footer(text=f"{uid}", icon_url=f"{Interaction.user.avatar}")
