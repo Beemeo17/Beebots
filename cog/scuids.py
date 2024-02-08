56d787a311bcd8648d84505017a9cbe32d8ba64a
Beemeo17 <127359555+Beemeo17@users.noreply.github.com>
2024-02-08 22:22:51 +0700

    scuids.py

--- cog/scuids.py
+++ cog/scuids.py
@@ -53,6 +53,7 @@ async def ntscuid(nntsl):
     return urrl_nt[index[nntsl]]
     
 async def image_dcuid(charactert):
+                uid = global_data.get("uid")
                 data = global_data.get("data")
                 weapon = charactert.weapon                                    
                 urls_to_download = [
