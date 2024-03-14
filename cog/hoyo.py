4e78f59f664c82be99916b3efdfbfaabdd7d0770
Beemeo17 <127359555+Beemeo17@users.noreply.github.com>
2024-03-14 18:14:12 +0700

    hoyo.py

--- cog/hoyo.py
+++ cog/hoyo.py
@@ -39,9 +39,9 @@ async def download_images(urls):
 async def main_img(isd):
   async with aiohttp.ClientSession() as session: 
     imgoc = {
-      "GENSHIN": "https://media.discordapp.net/attachments/1107978903294853140/1212074878984003634/Khong_Co_Tieu_e166.png?ex=6602f8b2&is=65f083b2&hm=96f904d237652e2de852a90e0f8043d8397b4b39298b3a560ede25c9ed0eb771&a",
-      "STARRAIL": "https://media.discordapp.net/attachments/1107978903294853140/1212073816906534954/Khong_Co_Tieu_e166.png?ex=65f082b5&is=65de0db5&hm=cfcadc40e113843b842b12518ee0310d74e119938317a343708ee246bd39d0d3&a",
-      "HONKAI": "https://media.discordapp.net/attachments/1107978903294853140/1212074966955204698/Khong_Co_Tieu_e166.png?ex=65f083c7&is=65de0ec7&hm=cc1c4f3867745fe6e3a0544313c59171ca669ccc5298913501ac86b9f346010b&a",
+      "GENSHIN": "https://iili.io/JWOq5kQ.png",
+      "STARRAIL": "https://iili.io/JWOq7pV.png",
+      "HONKAI": "https://iili.io/JWOqaTB.png",
     }
     icus = await fetch_image(session, imgoc[isd])
     return icus
@@ -56,11 +56,11 @@ async def upjs(self, Interaction, bu) -> None:
     user_id = str(Interaction.user.id)
     data = load_data()
     if data[user_id]["daily_auto"]:
-     self.auto_claim.label = "Disabled"
-     self.auto_claim.style=discord.ButtonStyle.red
+     self.children[bu].label = "Disabled"
+     self.children[bu].style=discord.ButtonStyle.red
     else:
-      self.auto_claim.label = "Enabled"
-      self.auto_claim.style=discord.ButtonStyle.green
+      self.children[bu].label = "Enabled"
+      self.children[bu].style=discord.ButtonStyle.green
   await Interaction.edit_original_response(view=self.Button())
 
 class button(discord.ui.View):
@@ -136,7 +136,7 @@ class button2(discord.ui.View):
 
 async def acdaily(image_app, has, claimed_rewards, x, y, i):
   async with aiohttp.ClientSession() as session: 
-      dailyt = await fetch_image(session, "https://media.discordapp.net/attachments/1107978903294853140/1212078877799874580/4C10BA33-37C5-4EA6-A901-1712C841D5C8.png?ex=6602fc6b&is=65f0876b&hm=3cbbe99385fa1fb267759f41f5c9525ca9bd78ab9633886d28cd347a9654bf53&a")
+      dailyt = await fetch_image(session, "https://iili.io/JWOqchP.jpg")
       daily_app = Image.open(BytesIO(dailyt)).resize((114, 151))
       dailyl = Image.open(BytesIO(await fetch_image(session, has.icon))).convert("RGBA").resize((102, 102))
       daily_app.paste(dailyl, (6, 10), mask=dailyl)
